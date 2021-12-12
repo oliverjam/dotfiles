81de03ad4a5d61eab24bd250f30a3cf7265f54b7
oliverjam <oliverjamesphillips@gmail.com>
2021-12-12 11:27:27 +0000

    Add git clone & cd function

--- .config/zsh/functions.zsh
+++ .config/zsh/functions.zsh
@@ -5,6 +5,13 @@ function mkcd() {
   mkdir -p "$@" && cd "$_";
 }
 
+# clone repo from URL
+# then grab the last bit of the URL (the directory)
+# and cd into it
+function clone() {
+  git clone "$1" && cd "$(basename "$1" .git)"
+}
+
 function newjs() {
   mkcd "$1" && npm init -y && git init && npx gitignore node
 }
